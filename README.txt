961bd554c5ecd6b44d7a48b6b9b7016ef023afec
mia-resendiz <160759884+mia-resendiz@users.noreply.github.com>
2024-06-14 02:56:28 +0000

    Initial commit

---
+++ README.txt
@@ -0,0 +1,6 @@
+inicio 
+Define dos variables con dos números enteros
+Compara los dos números para encontrar el mayor
+12 < 77
+numero mayor=77
+fin
---
+++ script.js
@@ -0,0 +1,12 @@
+var numero01;
+var numero02;
+numero01=parseInt(prompt("ingresa el numero 1",""));
+numero02=parseInt(prompt("ingresa el numero 2",""));
+if (numero01>numero02)
+{
+  document.write("El numero mayor es:" + numero01 +"(numero 1)");
+}
+else
+{
+  document.write("El numero mayor es:" + numero02 +"(numero 2)");
+}
